19de9786b943e5f50d5278beeb729f61d140b394
Angelo Siqueira <edmundosiqueira06@gmail.com>
2022-11-16 23:37:01 -0300

    Atualizado

--- main.js
+++ main.js
@@ -22,16 +22,24 @@ document.querySelector("#cards").innerHTML =
                 createCard(
                   "24/11",
                   "quinta",
-                  createGame("brasil", "16:00", "serbia")
+                  createGame("brasil", "16:00", "serbia")+
+                  createGame("portugal", "13:00", "ghana")+
+                  createGame("switzerland", "07:00", "cameroon")+
+                  createGame("koreia", "10:00", "uruguay")
                 )+
                 createCard(
                   "28/11",
                   "segunda",
                   createGame("switzerland", "13:00", "brasil") +
-                  createGame("portugal", "16:00", "uruguay")
+                  createGame("portugal", "16:00", "uruguay")+
+                  createGame("koreia", "10:00", "ghana")+
+                  createGame("cameroon", "7:00", "serbia")
                 )+
                 createCard(
                   "02/12",
                   " sexta",
-                  createGame("brasil", "16:00", "cameroon")
+                  createGame("brasil", "16:00", "cameroon")+
+                  createGame("uruguay", "12:00", "ghana")+
+                  createGame("serbia", "16:00", "switzerland")+
+                  createGame("koreia", "12:00", "portugal")
                 )
